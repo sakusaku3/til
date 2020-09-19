91d5fdffec8f688cc37a00f9d42007a0ff565b2d
sakusaku <haruichi33333@gmail.com>
2020-09-19 12:55:34 +0900

    pythonコード追加

--- python/excel/excel2csv.py
+++ python/excel/excel2csv.py
@@ -9,54 +9,54 @@ import openpyxl as px
 def main():
 
     args = sys.argv
-    exf = ''
-    csvf = ''
-    sheet = ''
+    excelFile = ''
+    csvFile = ''
+    sheetName = ''
 
     if len(args) < 2:
-        print ('USAGE : excel2csv -excel EXCELFILE -csv CSVFILE -sheet SHEETNAME')
+        print ('USAGE : excel2csv -excel EXCELFILE -csv CSVFILE -sheetName SHEETNAME')
         sys.exit()
 
     for i in range(len(args)):
         if args[i] == '-excel':
-            exf = args[i+1]
+            excelFile = args[i+1]
         if args[i] == '-csv':
-            csvf = args[i+1]
-        if args[i] == '-sheet':
-            sheet = args[i+1]
+            csvFile = args[i+1]
+        if args[i] == '-sheetName':
+            sheetName = args[i+1]
 
-    exfilepath = os.path.abspath(exf)
+    exfilepath = os.path.abspath(excelFile)
 
     if os.path.exists(exfilepath) == False:
-        print ('!!!ERROR!!! EXCEL FILE', exf , ' is not found')
+        print ('!!!ERROR!!! EXCEL FILE', excelFile, ' is not found')
         sys.exit()
 
-    bk = px.load_workbook(exfilepath, read_only=True, keep_vba=False)
+    workBook = px.load_workbook(exfilepath, read_only=True, keep_vba=False)
 
     flg = 0
-    for sh in bk.sheetnames:
-        if sh == sheet:
+    for sn in workBook.sheetnames:
+        if sn == sheetName:
             flg = 1
             break
 
     if flg == 0:
-        print ('!!!ERROR!!! SHEET NAME', sheet , ' is not found')
+        print ('!!!ERROR!!! SHEET NAME', sheetName , ' is not found')
         sys.exit()
 
-    if csvf == '':
-        csvf = exfilepath + '.' + sheet + '.csv'
+    if csvFile == '':
+        csvFile = exfilepath + '.' + sheetName + '.csv'
     else:
-        csvf = os.path.dirname(exfilepath) + '/' + csvf
+        csvFile = os.path.dirname(exfilepath) + '/' + csFilef
 
-    print ('EXCELFILE : ',exfilepath)
-    print ('SHEETNAME : ',sheet)
-    print ('CSVFILE : ',csvf)
+    print ('EXCELFILE : ', exfilepath)
+    print ('SHEETNAME : ', sheetName)
+    print ('CSVFILE : ', csvFile)
 
-    ws = bk[sheet]
+    workSheet = workBook[sheetName]
 
-    with open(csvf, 'w', encoding='utf-8') as fp:
+    with open(csvFile, 'w', encoding='utf-8') as fp:
         writer = csv.writer(fp)
-        for cols in ws.rows:
+        for cols in workSheet.rows:
             writer.writerow([str(col.value or '') for col in cols])
 
     print ('FINISHED')
